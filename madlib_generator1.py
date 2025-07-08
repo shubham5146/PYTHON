8ad4d030fdbc88142e58c228921db33a4436c4bd
Shubham5146 <nirjaladuwal99@gmail.com>
2025-07-08 18:06:36 +0545

    changed

--- madlib_generator1.py
+++ madlib_generator1.py
@@ -1,5 +1,5 @@
 adj1 = input("Enter a adjective: ")
-adjective2 = input("Enter another adjective: ")
+aj2 = input("Enter another adjective: ")
 type_of_bird = input("Enter type of a bird: ")
 room_in_a_house = input("Enter a room in a house: ")
 verb1_past_tense = input("Enter a verb in past tense: ")
@@ -15,7 +15,7 @@ noun2 = input("Enter another noun: ")
 
 
 story = f'It was a {adj1}, cold November day. I woke up to the \
- {adjective2} smell of {type_of_bird} roasting in the {room_in_a_house}\
+ {aj2} smell of {type_of_bird} roasting in the {room_in_a_house}\
  downstairs. I {verb1_past_tense} down the stairs to see if I could help\
  {verb2} the dinner. My mom said, "See if {relative_name} needs a fresh\
  {noun1}." So I carried a tray of glasses full of {a_liquid} into the \
